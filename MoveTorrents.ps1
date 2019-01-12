56c7fa8868e950592cf740fbb68e7441852b119f
aasims11 <43349146+aasims11@users.noreply.github.com>
2019-01-12 00:43:17 -0600

    Combined code into a smaller function with a hash table for lookups

--- MoveTorrents.ps1
+++ MoveTorrents.ps1
@@ -1,33 +1,86 @@
-﻿#$(
+#$(
 #env vars
 $stageDir='E:\Stage'
 $sDir='E:\Videos\Series'
 $sourceDir='D:\Torrents'
 $kidsSDir='E:\Videos\Kids Shows'
+
+#hash table to translate diffs with filenames vs directories using wildcards
+$showName= @{simpsons = 'simpsons';  burgers = 'bobs burgers';sheldon = "Young Sheldon"; family = 'Family Guy'; theory = 'Big Bang Theory'; goldbergs = 'goldbergs'; dad = 'American Dad'}
+$seriesList = Get-ChildItem 'E:\videos\Series'
 #create list of torrents that have been downloaded last day and save the name to a var
-$list=Get-ChildItem $sourceDir | Where-Object {$_.LastWriteTime -gt (Get-Date).AddDays(-7)} | Select-Object -ExpandProperty Name
-
-
+$list=Get-ChildItem $sourceDir | Where-Object {$_.LastWriteTime -gt (Get-Date).AddDays(-4)} | Select-Object -ExpandProperty Name
+#check for dirs
+Write-Output $seriesList | Select-Object -ExpandProperty Name
 #blocks to wrap around script body to output to log
 # 
 #
-
-
-
+#MatchShow loops through keys in the hashtable from the filelist in dropzone
+Function MatchShow($f){
+  foreach ($s in $showName.Keys){
+   
+  if ($f -like "*$s*"){
+    Write-Output "Found Match in Series"
+    $show = $showName.$s
+    Write-Output "Copying $f to $sDir\$show"
+    Copy-Item "$stageDir\Dropzone\$f" "$sDir\$show" -Force
+    $result= Test-Path "$sDir\$show\$f"
+     
+    if ($result -eq 'true'){
+    Write-Output "$f moved to the correct folder`n"
+    Write-output "Removing $f from Staging Directory"
+    Remove-Item "$stageDir\Dropzone\$f" 
+    Write-output "$f is still present in Dropzone:" 
+    Test-Path "$stageDir\Dropzone\$f"
+    Write-output "`n"
+    }
+    else{
+    Write-Output "$f did not copy to $sDir\$show`n"
+    }
+    
+}
+    
+}
+}#/function
  Function ExtractFiles(){
  #for each filename returned unzip to stage and overwrite if present
     foreach ($l in $list){
     Write-Output "This file is being extracted $l"
-    7z.exe x $sourceDir\$l\$l.rar -o"$stageDir\" -aoa
+    7z.exe x $sourceDir\$l\$l.rar -o"$stageDir\Dropzone" -aoa
    }
 
  }#/function
-
+#Actions (main)
+ ExtractFiles
+ #get list of files in dropzone from extract job
+$files = Get-ChildItem "E:\Stage\Dropzone" | Select-Object -ExpandProperty Name 
+Write-Output "filelist:`n $files"
+foreach ($f in $files){
+ MatchShow($f)
+}
+$files = Get-ChildItem "E:\Stage\Dropzone" | Select-Object -ExpandProperty Name
+if ($files.Count -gt 1){
+  foreach ($f in $files){
+    Write-Output "No Match - $f moving to Undefined`n"
+    Copy-Item "$stageDir\Dropzone\$f" "E:\Stage\Undefined" 
+    $r=Test-Path "E:\Stage\Undefined\$f"
+     if ( $r -eq 'True'){
+      Write-output "Removing $f from Staging Directory"
+      Remove-Item "$stageDir\Dropzone\$f" 
+      Test-Path "$stageDir\Dropzone\$f"
+     }
+    else{
+      write-out "$f did not copy - Breaking operation"
+      exit(1)
+    }
+  }
+}
+ #Old Logic using if-else
  Function SeriesCheck(){
  #Examine file and try to match against current dir
 
-$files = Get-ChildItem E:\Stage | Select-Object -ExpandProperty Name 
-Write-Output "filelist:`n $files"
+$files = Get-ChildItem "E:\Stage\Dropzone" | Select-Object -ExpandProperty Name 
+Write-Output "filelist:`n $files`n"
 #for each file check against known shows and move to the correct series folder
 foreach ($f in $files){
 
@@ -35,14 +88,14 @@ foreach ($f in $files){
 if ($f -like "*Young*Sheldon*"){
    Write-Output "Found Young Sheldon"
    Write-Output "Copying $f to $sDir\Young Sheldon"
-   Copy-Item "$stageDir\$f" "$sDir\Young Sheldon" -Force
+   Copy-Item "$stageDir\Dropzone\$f" "$sDir\Young Sheldon" -Force
    $result= Test-Path "$sDir\Young Sheldon\$f"
     
    if ($result -eq 'true'){
    Write-Output "$f moved to the correct folder`n"
    Write-output "Removing $f from Staging Directory"
-   Remove-Item "$stageDir\$f" 
-   Test-Path "$stageDir\$f"
+   Remove-Item "$stageDir\Dropzone\$f" 
+   Test-Path "$stageDir\Dropzone\$f"
    }
    else{
    Write-Output "$f did not copy to $sDir\Young Sheldon`n"
@@ -51,14 +104,14 @@ if ($f -like "*Young*Sheldon*"){
   elseif ($f -like "*Family*Guy*"){
   Write-Output "Found Family Guy"
   Write-Output "Copying $f to $sDir\Family Guy"
-  Copy-Item "$stageDir\$f" "$sDir\Family Guy" -Force
+  Copy-Item "$stageDir\Dropzone\$f" "$sDir\Family Guy" -Force
   $result= Test-Path "$sDir\Family Guy\$f"
    
   if ($result -eq 'true'){
   Write-Output "$f moved to the correct folder`n"
   Write-output "Removing $f from Staging Directory"
-  Remove-Item "$stageDir\$f" 
-  Test-Path "$stageDir\$f"
+  Remove-Item "$stageDir\Dropzone\$f" 
+  Test-Path "$stageDir\Dropzone\$f"
   }
   else{
   Write-Output "$f did not copy to $sDir\Family Guy`n"
@@ -67,14 +120,14 @@ if ($f -like "*Young*Sheldon*"){
  elseif ($f -like "*Big*Bang*"){
    Write-Output "Found Big Bang"
    Write-Output "Copying $f to $sDir\Big Bang Theory"
-   Copy-Item "$stageDir\$f" "$sDir\Big Bang Theory" -Force
+   Copy-Item "$stageDir\Dropzone\$f" "$sDir\Big Bang Theory" -Force
    $result= Test-Path "$sDir\Big Bang Theory\$f"
 
    if ($result -eq 'true'){
    Write-Output "$f moved to the correct folder`n" 
    Write-output "Removing $f from Staging Directory"
-   Remove-Item "$stageDir\$f" 
-   Test-Path "$stageDir\$f"
+   Remove-Item "$stageDir\Dropzone\$f" 
+   Test-Path "$stageDir\Dropzone\$f"
    }
    else{
    Write-Output "$f did not copy to $sDir\Big Bang Theory`n"
@@ -84,49 +137,81 @@ if ($f -like "*Young*Sheldon*"){
    elseif ($f -like "*Goldbergs*"){
    Write-Output "Found Goldbergs"
    Write-Output "Copying $f to $sDir\Goldbergs"
-   Copy-Item "$stageDir\$f" "$sDir\Goldbergs" -Force
+   Copy-Item "$stageDir\Dropzone\$f" "$sDir\Goldbergs" -Force
    $result= Test-Path "$sDir\Goldbergs\$f"
 
    if ($result -eq 'true'){
    Write-Output "$f moved to the correct folder`n" 
    Write-output "Removing $f from Staging Directory"
-   Remove-Item "$stageDir\$f" 
-   Test-Path "$stageDir\$f"
+   Remove-Item "$stageDir\Dropzone\$f" 
+   Test-Path "$stageDir\Dropzone\$f"
    }
    else{
    Write-Output "$f did not copy to $sDir\Goldbergs`n"
    }
    }
+   elseif ($f -like "*simpsons*"){
+    Write-Output "Found Simpsons"
+    Write-Output "Copying $f to $sDir\simpsons"
+    Copy-Item "$stageDir\Dropzone\$f" "$sDir\simpsons" -Force
+    $result= Test-Path "$sDir\simpsons\$f"
+ 
+    if ($result -eq 'true'){
+    Write-Output "$f moved to the correct folder`n" 
+    Write-output "Removing $f from Staging Directory"
+    Remove-Item "$stageDir\Dropzone\$f" 
+    Test-Path "$stageDir\Dropzone\$f"
+    }
+    else{
+    Write-Output "$f did not copy to $sDir\Simpsons`n"
+    }
+    }
+    elseif ($f -like "*bobs*burgers*"){
+      Write-Output "Found Bobs Burgers"
+      Write-Output "Copying $f to $sDir\Bobs Burgers"
+      Copy-Item "$stageDir\Dropzone\$f" "$sDir\Bobs Burgers" -Force
+      $result= Test-Path "$sDir\Bobs Burgers\$f"
+   
+      if ($result -eq 'true'){
+      Write-Output "$f moved to the correct folder`n" 
+      Write-output "Removing $f from Staging Directory"
+      Remove-Item "$stageDir\Dropzone\$f" 
+      Test-Path "$stageDir\Dropzone\$f"
+      }
+      else{
+      Write-Output "$f did not copy to $sDir\Bobs Burgers`n"
+      }
+      }
    #kids shows
    elseif ($f -like "*Ducktales*"){
    Write-Output "Found Ducktales"
    Write-Output "Copying $f to $kidsSDir\Ducktales"
-   Copy-Item "$stageDir\$f" "$kidsSDir\Ducktales 2017" -Force
+   Copy-Item "$stageDir\Dropzone\$f" "$kidsSDir\Ducktales 2017" -Force
    $result= Test-Path "$kidsSDir\Ducktales 2017\$f"
 
    if ($result -eq 'true'){
    Write-Output "$f moved to the correct folder`n" 
    Write-output "Removing $f from Staging Directory"
-   Remove-Item "$stageDir\$f" 
-   Test-Path "$stageDir\$f"
+   Remove-Item "$stageDir\Dropzone\$f" 
+   Test-Path "$stageDir\Dropzone\$f"
    }
    else{
    Write-Output "$f did not copy to $sDir\Ducktales`n"
    }
    }
+   
    else {
-     Copy-Item "$stageDir\$f" "$stageDir\Undefined" 
-     Write-Output "No Match - $f moving to Undefined`n"
+     Copy-Item "$stageDir\Dropzone\$f" "E:\Stage\Undefined" 
+     Write-Output "No Match - $f moved to Undefined`n"
      Write-output "Removing $f from Staging Directory"
-     Remove-Item "$stageDir\$f" 
-     Test-Path "$stageDir\$f"
+     Remove-Item "$stageDir\Dropzone\$f" 
+     Test-Path "$stageDir\Dropzone\$f"
    }
   }
  }#/function
+ 
+ #SeriesCheck
 
-
-
- ExtractFiles
-
- SeriesCheck
+ 
+ 
  #) *>&1 > $stageDir\logs\log.txt
